4b6839624d17d2c611de138abcffa57a1efe422f
shri030306 <shrilakshmigs03@gmail.com>
2025-10-18 12:53:59 +0530

    save

--- CIE1.py
+++ CIE1.py
@@ -1,1 +1,24 @@
-a=(input("enter "))
+# Input from user
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
+
+# Find the greatest number
+if a >= b and a >= c:
+    greatest = a
+elif b >= a and b >= c:
+    greatest = b
+else:
+    greatest = c
+
+# Find the smallest number
+if a <= b and a <= c:
+    smallest = a
+elif b <= a and b <= c:
+    smallest = b
+else:
+    smallest = c
+
+# Display results
+print("Greatest number is:", greatest)
+print("Smallest number is:", smallest)
